0a0185388e574be581ffac4ceb78850d46d2864e
SerenityV8 <aubert.guillaume26@gmail.com>
2021-11-16 08:47:43 +0100

    Fixing Google Analytics

---
+++ src/lib/Analytics.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { getStores } from "$app/stores";
+  const { page } = getStores();
+  $: {
+    if (typeof gtag !== "undefined"){
+      gtag("config", "UA-SOMEANALYTICSID-1", {
+        page_path: $page.path
+      });
+    }
+  }
+</script>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,12 @@
 <script>
     import "../global.css";
+    import Analytics from "$lib/Analytics.svelte"
     import Header from "$lib/Header.svelte";
     import Information from "$lib/Information.svelte";
     import Footer from "$lib/Footer.svelte";
 </script>
 
+<Analytics />
 <Header />
 <slot />
 <Information />
